b2db834b302bc640f1b364884fec08d55fdf8f4e
Bryan Ngo <bngo92@gmail.com>
2019-05-28 16:54:04 -0700

    Update install-consul.sh

--- consul-on-ubuntu/install-consul.sh
+++ consul-on-ubuntu/install-consul.sh
@@ -2,11 +2,63 @@
 
 sudo apt-get update && sudo apt-get install -y wget unzip
 
-sudo mkdir -p /opt/consul/data
-cd /opt/consul
-#sudo wget https://releases.hashicorp.com/consul/0.6.3/consul_0.6.3_linux_amd64.zip
-sudo wget https://releases.hashicorp.com/consul/0.7.0/consul_0.7.0_linux_amd64.zip
-sudo unzip consul_0.7.0_linux_amd64.zip
-sudo chmod 755 consul
-cd /opt/consul
-sudo nohup ./consul agent -server -bind 0.0.0.0 -client 0.0.0.0 -data-dir="/opt/consul/data" -bootstrap-expect 3 -atlas=$1 -atlas-join -atlas-token="$2" &
+# https://learn.hashicorp.com/consul/datacenter-deploy/deployment-guide
+CONSUL_VERSION="1.2.0"
+curl --silent --remote-name https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
+curl --silent --remote-name https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_SHA256SUMS
+curl --silent --remote-name https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_SHA256SUMS.sig
+
+unzip consul_${CONSUL_VERSION}_linux_amd64.zip
+sudo chown root:root consul
+sudo mv consul /usr/local/bin/
+consul --version
+
+consul -autocomplete-install
+complete -C /usr/local/bin/consul consul
+
+sudo useradd --system --home /etc/consul.d --shell /bin/false consul
+sudo mkdir --parents /opt/consul
+sudo chown --recursive consul:consul /opt/consul
+
+sudo touch /etc/systemd/system/consul.service
+
+echo "[Unit]
+Description="HashiCorp Consul - A service mesh solution"
+Documentation=https://www.consul.io/
+Requires=network-online.target
+After=network-online.target
+ConditionFileNotEmpty=/etc/consul.d/consul.hcl
+
+[Service]
+User=consul
+Group=consul
+ExecStart=/usr/local/bin/consul agent -config-dir=/etc/consul.d/
+ExecReload=/usr/local/bin/consul reload
+KillMode=process
+Restart=on-failure
+LimitNOFILE=65536
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/consul.service
+
+sudo mkdir --parents /etc/consul.d
+sudo touch /etc/consul.d/consul.hcl
+sudo chown --recursive consul:consul /etc/consul.d
+sudo chmod 640 /etc/consul.d/consul.hcl
+
+echo 'datacenter = "dc1"
+data_dir = "/opt/consul"
+encrypt = "Luj2FZWwlt8475wD1WtwUQ=="' > /etc/consul.d/consul.hcl
+
+sudo mkdir --parents /etc/consul.d
+sudo touch /etc/consul.d/server.hcl
+sudo chown --recursive consul:consul /etc/consul.d
+sudo chmod 640 /etc/consul.d/server.hcl
+
+echo "server = true
+bootstrap_expect = 3
+ui = true" > /etc/consul.d/server.hcl
+
+sudo systemctl enable consul
+sudo systemctl start consul
+sudo systemctl status consul
